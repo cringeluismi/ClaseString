bc6cc4c78c2884e2a2741df786d1f538bb86b0b2
Luis <luibenmor@gmail.com>
2023-11-22 09:50:00 +0100

    commit

--- src/CifradoCesar.java
+++ src/CifradoCesar.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class CifradoCesar {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Frase:");
+        String userInput = sc.nextLine().toUpperCase().replace(" ","");
+        char sinCifrado[] = userInput.toCharArray();
+        System.out.println(sinCifrado);
+
+        System.out.println("Clave:");
+        int clave = sc.nextInt();
+
+        for (int i = 0; i < sinCifrado.length; i++) {
+            sinCifrado[i] += clave;
+        }
+
+        System.out.println(sinCifrado);
+
+
+
+
+
+
+
+
 
     }
 }
