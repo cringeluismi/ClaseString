452789bd09d66e9499d12b9ac7cd89fb11034c2f
Luis <luibenmor@gmail.com>
2023-12-04 13:07:38 +0100

    commit

--- src/PracString2.java
+++ src/PracString2.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class PracString2 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        String fraseUsuario = sc.nextLine();
+        char charArray[] = new char[fraseUsuario.length()];
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[0] != ' ' || charArray[i+1] != ' ') {
+                charArray[i] == charArray[i].toUpperCase();
+            }
+        }
 
     }
 }
